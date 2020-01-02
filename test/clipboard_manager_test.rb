8f03fff0905982108c8a3532e69d8dbae0ae49bc
satoshi kawamoto <s.kawamoto.contact@gmail.com>
2020-01-02 17:48:16 +0900

    test-unitに変更

--- test/clipboard_manager_test.rb
+++ test/clipboard_manager_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class ClipboardManagerTest < Minitest::Test
+class ClipboardManagerTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::ClipboardManager::VERSION
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "clipboard_manager"
 
-require "minitest/autorun"
+require "test/unit"
