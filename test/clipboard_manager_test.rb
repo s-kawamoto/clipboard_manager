f565def6cf96bbb07179eb5acd5e33158effbca4
satoshi kawamoto <s.kawamoto.contact@gmail.com>
2020-01-02 17:49:29 +0900

    不要なテストを削除

--- test/clipboard_manager_test.rb
+++ test/clipboard_manager_test.rb
@@ -1,11 +1,4 @@
 require "test_helper"
 
 class ClipboardManagerTest < Test::Unit::TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::ClipboardManager::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
